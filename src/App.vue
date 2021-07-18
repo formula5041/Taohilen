c4e42f710f564fa181d9890df3fc2656c85087e4
formula5041 <formula5041@gmail.com>
2021-07-18 18:16:37 +0800

    '開專案'

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <the-header/>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+  }
+}
+</script>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-gray-500 absolute w-10/12 h-5/6">
+        <img class="w-20 absolute" src="../assets/img/icon/crab_white.svg">
+        <img class="w-full h-full" src="../assets/img/banner/port.jpg">
+    </div>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
