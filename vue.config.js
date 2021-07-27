c7f4cf427ec8380606d8543d8ac9d1aa0d9fc2e5
formula5041 <formula5041@gmail.com>
2021-07-27 14:44:34 +0800

    'Github部屬'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'Taohilen'
-      ? '/eric-project/'
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/Taohilen/'
       : '/'
   }
